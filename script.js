0e0b936f6db496f5808341eb819231af6f2b8451
Alex <lesha.rudiko@gmail.com>
2024-04-06 16:59:05 +0300

    new branch

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 "use strict"
 
-const getData = () => {
-return fetch('db.json')
-    .then(res => res.json())
+const sendData = (data) => {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('POST', "https://jsonplaceholder.typicode.com/posts")
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    xhr.onload = () => {
+        if(xhr.status < 205){
+            console.log(xhr.response);
+        } else{
+            console.error("ошибка запроса " + xhr.status);
+        }
+    }
+
+    xhr.send(JSON.stringify(data))
 }
 
-const sendData = (user) => {
-    return fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(user)
-    }).then(res => res.json())
+const getData = () => {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'db.json')
+
+    xhr.onload = () => {
+        if(xhr.statusText === 'OK'){
+            const data = JSON.parse(xhr.response)
+            sendData(data)
+        } else{
+            console.error("ошибка запроса" + xhr.status);
+        }
+    }
+
+    xhr.send()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-        getData()
-            .then(data => {
-                sendData(data)
-                    .then(data => {
-                        console.log(data)
-                        console.log('отправка успешна');
-                    })
-                    .catch(() => console.error())
-            })
-            .catch(() => console.error())
+    getData()
 })
